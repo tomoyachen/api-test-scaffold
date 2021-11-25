592438338fb350248077bcc3f54852e5cbd6c47d
tomoyachen <tomoya_chen@163.com>
2021-11-25 22:31:45 +0800

    feat(): 接口输出日志

--- common/request.py
+++ common/request.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import logging
+import traceback
 
 class Request():
     def __init__(self, cookies = None):
@@ -20,17 +22,21 @@ class Request():
         requests.packages.urllib3.disable_warnings()
         requests.adapters.DEFAULT_RETRIES = 5
 
+        self.method = self.method.upper()
+        self.url = self.url if self.url else self.host + self.path
+
         s = requests.session()
         s.keep_alive = False
         self.response = s.request(
             self.method,
-            url=self.url if self.url else self.host + self.path,
+            url=self.url,
             headers=self.headers,
             params=self.params,
             data=json.dumps(self.data),
             cookies=self.cookies,
             verify=False,
         )
+        self.__log()
 
         return self.response
 
@@ -47,3 +53,18 @@ class Request():
         if expect_message  is not None:
             assert expect_message in self.response.json()["json"]["message"], f"message 不一致"
 
+
+    def __log(self):
+        url_with_params = self.url
+        if self.params:
+            path = "&".join([f'{key}={self.params[key]}' for key in self.params])
+            url_with_params += f"&{path}" if "?" in url_with_params else f"?{path}"
+
+        log = logging.getLogger()
+        log.info(f"""Caller: {traceback.extract_stack()[-3].__str__()}
+{self.method}  {url_with_params}
+Request Headers: {self.headers}
+Request Body: {self.data}
+HTTP Code: {self.response.status_code}
+Response Body: {self.response.text}"""
+                 )
